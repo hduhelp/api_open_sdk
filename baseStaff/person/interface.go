fd54d7907d1ac9bb539c8d87fa100a70d26be23c
FDKevin <fdkevinzero@gmail.com>
2021-11-29 22:51:57 +0800

    feat(person): update person info interface

--- baseStaff/person/interface.go
+++ baseStaff/person/interface.go
@@ -1,12 +1,5 @@
 package person
 
-import "github.com/hduhelp/api_open_sdk/baseStaff"
-
 type InfoInterface interface {
-	GetStaffID() string
-	GetStaffName() string
-	GetStaffType() baseStaff.Type
-	GetStaffState() string
-	GetUnitCode() string
-	GetUnitName() string
+	GetPersonInfo() (*Info, error)
 }
