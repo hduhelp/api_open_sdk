49702576baa5ad13683a15a7bcab7ec6401b8620
FDKevin <fdkevinzero@gmail.com>
2022-03-16 13:35:40 +0800

    feat(staffType): update

--- baseStaff/gin.go
+++ baseStaff/gin.go
@@ -6,23 +6,9 @@ func FromContext(c *gin.Context) *Staff {
 	staffID := c.GetHeader("staffId")
 	if staff, ok := map[string]*Staff{
 		"1": Undergraduate(staffID),
-		"2": Teacher(staffID),
-		"3": Postgraduate(staffID),
+		"2": Postgraduate(staffID),
+		"3": Teacher(staffID),
 	}[c.GetHeader("staffType")]; ok {
-		return staff
-	}
-
-	if staff, ok := map[string]*Staff{
-		"10": Undergraduate(staffID),
-		"11": Undergraduate(staffID),
-		"30": Teacher(staffID),
-		"31": Teacher(staffID),
-		"20": Postgraduate(staffID),
-		"21": Postgraduate(staffID),
-
-		"":   {ID: staffID, Type: Type_Unknown},
-		"90": {ID: staffID, Type: Type_Unknown},
-	}[c.GetHeader("userType")]; ok {
 		return staff
 	}
 
