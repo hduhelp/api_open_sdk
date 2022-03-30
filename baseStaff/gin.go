45d2d48868116fb8fdf1b1f6688213ec55d9fcff
fdkevin <fdkevinzero@gmail.com>
2022-03-30 11:03:14 +0800

    feat(staff): staff from header

--- baseStaff/gin.go
+++ baseStaff/gin.go
@@ -1,6 +1,10 @@
 package baseStaff
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func FromContext(c *gin.Context) *Staff {
 	staffID := c.GetHeader("staffId")
@@ -13,4 +17,17 @@ func FromContext(c *gin.Context) *Staff {
 	}
 
 	return c.MustGet("staff").(*Staff)
+}
+
+func FromHeader(header http.Header) *Staff {
+	staffID := header.Get("staffId")
+	if staff, ok := map[string]*Staff{
+		"1": Undergraduate(staffID),
+		"2": Postgraduate(staffID),
+		"3": Teacher(staffID),
+	}[header.Get("staffType")]; ok {
+		return staff
+	}
+
+	return &Staff{ID: staffID}
 }
