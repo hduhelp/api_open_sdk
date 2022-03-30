a796a949d35be3e7add010c01ec7490e3488afcb
fdkevin <fdkevinzero@gmail.com>
2022-03-30 18:09:44 +0800

    feat(status): add campusapis status

---
+++ campusapis/status.go
@@ -0,0 +1,37 @@
+package campusapis
+
+import (
+	"strings"
+
+	"google.golang.org/grpc/codes"
+)
+
+func (x Status) Warp(status codes.Code) codes.Code {
+	return WarpCode(status, uint32(x))
+}
+
+func (x Status) Message() string {
+	return strings.Replace(strings.ToLower(x.String()), "_", " ", -1)
+}
+
+func WarpCode(code codes.Code, errorCode uint32) codes.Code {
+	return code*100000 + codes.Code(errorCode)
+}
+
+type Codes struct {
+	Status   Status
+	GrpcCode codes.Code
+}
+
+func UnwarpCode(code codes.Code) Codes {
+	if code < 10000 {
+		return Codes{
+			Status:   Status_OK,
+			GrpcCode: code,
+		}
+	}
+	return Codes{
+		Status:   Status(int32(code) % 100000),
+		GrpcCode: code / 100000,
+	}
+}
