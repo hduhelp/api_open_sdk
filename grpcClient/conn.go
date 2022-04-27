24f091dda535377befeb1aae007ac2d3298ec849
fdkevin <fdkevinzero@gmail.com>
2022-04-27 18:39:31 +0800

    feat(grpcclient): default conn

---
+++ grpcClient/conn.go
@@ -0,0 +1,31 @@
+package grpcclient
+
+import (
+	"context"
+	"crypto/x509"
+	"log"
+
+	"github.com/samber/lo"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var defaultEndpoint = "gapi.hduhelp.com:443"
+
+func Conn(ctx context.Context, endpoints ...string) grpc.ClientConnInterface {
+	endpoint := lo.Ternary(len(endpoints) != 0,
+		endpoints[0],
+		defaultEndpoint,
+	)
+	certPool, err := x509.SystemCertPool()
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+	creds := credentials.NewClientTLSFromCert(certPool, "gapi.hduhelp.com")
+	conn, err := grpc.DialContext(ctx, endpoint, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal("grpc client did not connect", zap.Error(err))
+	}
+	return conn
+}
