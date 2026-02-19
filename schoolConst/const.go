b6b93e785e28773e45688881c004d62a0fc9c923
Jiahe114514 <your-email@example.com>
2026-02-19 23:00:48 +0800

    fix: timestamp

--- schoolConst/const.go
+++ schoolConst/const.go
@@ -8,7 +8,7 @@ import (
 const (
 	SchoolYear             = 2025
 	Semester               = 2
-	SemesterStartTimestamp = int64(1773504000) // 2026-3-15 00:00:00
+	SemesterStartTimestamp = int64(1772640000) // 2026-3-05 00:00:00
 )
 
 func getWeekNum() int {
