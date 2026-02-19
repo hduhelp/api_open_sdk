d6fc5470f99c3fafaef771fbdb1ce6d93d97a113
Jiahe114514 <your-email@example.com>
2026-02-19 22:23:30 +0800

    feat: semester

--- schoolConst/const.go
+++ schoolConst/const.go
@@ -7,8 +7,8 @@ import (
 
 const (
 	SchoolYear             = 2025
-	Semester               = 1
-	SemesterStartTimestamp = int64(1757865600) // 2025-9-15 00:00:00
+	Semester               = 2
+	SemesterStartTimestamp = int64(1773504000) // 2026-3-15 00:00:00
 )
 
 func getWeekNum() int {
